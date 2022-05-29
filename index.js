5f7ff64284f60c2feff3f497d98aa8bc13968f96
KirillTabachnikov <tabachnikovkg@yandex.ru>
2022-05-29 22:21:41 +0300

    дз3

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
+const GET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+const GET_BASKET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
+
+function service(url, callback) {
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onload = () => {
+    callback(JSON.parse(xhr.response))
+  }
+}
+
 class GoodsItem {
-  constructor({ title, price }) {
-    this.title = title;
+  constructor({ product_name, price }) {
+    this.product_name = product_name;
     this.price = price;
   }
   render() {
     return `
     <div class="goods-item">
       <img src="../HTMLCSS/cours2/img/Catalog22.png" alt="IMG">
-      <h3>${this.title}</h3>
+      <h3>${this.product_name}</h3>
       <p>${this.price}</p>
     </div>
   `;
@@ -22,8 +34,11 @@ class GoodsItem {
 }
 class GoodsList {
   items = [];
-  fetchGoods() {
-    this.items = goods;
+  fetchGoods(callback) {
+    service(GET_GOODS_ITEMS, (data) => {
+      this.items = data;
+      callback()
+    });
   }
 
   sumPrice() {
@@ -48,5 +63,6 @@ class GoodsList {
 
 const goodsList = new GoodsList();
 const basketSum = goodsList.sumPrice();
-goodsList.fetchGoods();
-goodsList.render();
+goodsList.fetchGoods(() => {
+  goodsList.render();
+});
