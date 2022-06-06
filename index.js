3351fc0550aed7669a26aa8ea235b74fb6798e7c
KirillTabachnikov <tabachnikovkg@yandex.ru>
2022-06-06 22:39:13 +0300

    ДЗ5

--- index.js
+++ index.js
@@ -10,76 +10,94 @@ const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
 const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`
 
 function service(url) {
-  return new Promise((resolve) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onload = () => {
-      if (xhr.readyState === 4) {
-        resolve(JSON.parse(xhr.response))
+  return fetch(url)
+    .then((res) => res.json())
+}
+
+
+service(GET_BASKET_GOODS_ITEMS).then((data) => {
+
+})
+
+
+function init() {
+  const CustomButton = Vue.component('custom-button', {
+    template: `
+      <button class="search-button" type="button" v-on:click="$emit('click')">
+         <slot></slot>
+      </button>
+    `
+  })
+  const basketGoods = Vue.component('basket-goods', {
+    data() {
+      return {
+        basketGoodsItems: []
       }
+    },
+
+    template: `
+      <div class="fixed-area">
+         <div class="basket-card">
+            <div class="basket-card__header">
+               <h1 class="basket-card__header__title">Корзина</h1>
+               <div class="basket-card__header__delete-icon"
+                  v-on:click="$emit('closeclick')"
+               ></div>
+            </div>
+            <div class="basket-card__content">
+               Содержимое
+            </div>
+         </div>
+      </div>
+    `
+  })
+
+  const goodsItem = Vue.component('goods-item', {
+    props: [
+      'item'
+    ],
+    template: `
+      <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+      </div>
+    `
+  })
+
+  const app = new Vue({
+    el: '#root',
+    data: {
+      items: [],
+      filteredItems: [],
+      search: '',
+      cardIsVision: false
+    },
+    methods: {
+      setVisionCard() {
+        this.cardIsVision = !this.cardIsVision
+      },
+      fetchGoods() {
+        service(GET_GOODS_ITEMS).then((data) => {
+          this.items = data;
+          this.filteredItems = data;
+        });
+      },
+      filterItems() {
+        this.filteredItems = this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
+      },
+    },
+    computed: {
+      calculatePrice() {
+        return this.filteredItems.reduce((prev, { price }) => {
+          return prev + price;
+        }, 0)
+      }
+    },
+    mounted() {
+      this.fetchGoods();
     }
   })
 }
-
-class GoodsItem {
-  constructor({ product_name, price }) {
-    this.product_name = product_name;
-    this.price = price;
-  }
-  render() {
-    return `
-    <div class="goods-item">
-      <img src="../HTMLCSS/cours2/img/Catalog22.png" alt="IMG">
-      <h3>${this.product_name}</h3>
-      <p>${this.price}</p>
-    </div>
-  `;
-  }
-}
-class GoodsList {
-  items = [];
-  filteredItems = [];
-  fetchGoods() {
-    const prom = service(GET_GOODS_ITEMS);
-    prom.then((data) => {
-      this.items = data;
-      this.filteredItems = data;
-    });
-
-    return prom;
-  }
-
-  sumPrice() {
-    const sum = this.items.reduce((previousValue, { price }) => previousValue + price, 0);
-    document.querySelector('.goods-sum').innerHTML = sum;
-  }
-
-
-
-  render() {
-    const goods = this.items.map(item => {
-      const goodItem = new GoodsItem(item);
-      return goodItem.render()
-    }).join('');
-
-    document.querySelector('.goods-list').innerHTML = goods;
-  }
-}
-
-class BasketGoodsList {
-  items = [];
-  fetchGoods() {
-    service(GET_BASKET_GOODS_ITEMS, (data) => {
-      this.items = data.contents;
-    });
-  }
-}
-
-const basketGoodsList = new BasketGoodsList();
-basketGoodsList.fetchGoods();
-const goodsList = new GoodsList();
-const basketSum = goodsList.sumPrice();
-goodsList.fetchGoods().then(() => {
-  goodsList.render();
-});
+window.onload = init
